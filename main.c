3b7b422d085aad56a9d6e167c1b6a0636ba8878a
Alan <ag.diaz.91@gmail.com>
2018-06-05 06:18:18 -0300

    Puesto pa que escriba en archivo o printee
    Está probado con las lineas 85: njson_print(&njson,NULL,0) y 86: njson_tofile(&njson , "testeo.txt")
    No lo probé con parámetros al main.
    Parece andar todo menos los array. El array del float latLong[2] = {37.77256666666666, -122.45934166666667}; imprime bien el primero 37.772 etc, pero el -122.4593 o imprime 0.000000 o imprimía -776.algo
    De hecho, en la foto que te pase ayer a la tarde ya imprimía eso, por ende ya andaba mal desde siempre supongo.
    No lo probé agregandole otro njson al array de jsons porque no quiero ver como explota.

--- main.c
+++ main.c
@@ -3,49 +3,94 @@
 #include <stdlib.h>
 #include "NJson.h"
 
-#define NAME_MAX 255 // Largo máximo para nombre de archivos en linux.
+#define NAME_MAX 255 // Largo maximo para nombre de archivos en linux.
 void main(int argc, char* argv[]) {
 
 	NJson njson;
-	char* escritura = 0x0;
+	NJson contents;
+	NJson photoInfo;
 
 	/**
 	 * Declaracion del puntero al nombre del archivo si lo necesitamos segun
 	 * opciones de ejecucion
-	*/
+	 */
 	char* nombre_archivo[NAME_MAX];
 
 	/**
 	 * Declaracion de variables a guardarse en el json para probar el funcionamiento
-	 *
-	*/
-	unsigned valorUnsigned = 55;
-	int valorInteger = 8;
-	double valorDouble = 11.28;
-	float valorFlotante = 15.12;
-	char valorBoleano = 1;
-	char* valorString = "714f129684fe";
+	 */
+
+	//photo_info
+	float latLong[2] = {37.77256666666666, -122.45934166666667};
+	char* timeTaken = "Wed, 28 Aug 2013 18:12:02 +0000";
+	njson_init(&photoInfo,"photo","lat_long",latLong,sizeof(latLong),2,(void*)&njson_imprimir_float);
+	njson_dato_agregar(&photoInfo,"time_taken",timeTaken,strlen(timeTaken)+1,0,(void*)&njson_imprimir_string);
+
+	//contents
+	char* size = "2.3 MB";
+	char* rev = "38af1b183490";
+	char thumbExists = '1';
+	unsigned bytes = 2453963;
+	char* modified = "Mon, 07 Apr 2014 23:13:16 +0000";
+	char* clientMtime = "Thu, 29 Aug 2013 01:12:02 +0000";
+	char* path = "/Photos/flower.jpg";
+	char isDir = '0';
+	char* icon = "page_white_picture";
+	char* root = "dropbox";
+	char* mimeType = "image/jpeg";
+	unsigned revision = 14511;
+
+	njson_init(&contents,"contents","size",size,strlen(size)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"rev",rev,strlen(rev)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"thumb_exists",&thumbExists,sizeof(char),0,(void*)&njson_imprimir_boolean);
+	njson_dato_agregar(&contents,"bytes",&bytes,sizeof(unsigned),0,(void*)&njson_imprimir_unsigned);
+	njson_dato_agregar(&contents,"modified",modified,strlen(modified)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"client_mtime",clientMtime,strlen(clientMtime)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"path",path,strlen(path)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"photo info",&photoInfo,sizeof(NJson),0,(void*)&njson_print);
+	njson_dato_agregar(&contents,"is_dir",&isDir,sizeof(char),0,(void*)&njson_imprimir_boolean);
+	njson_dato_agregar(&contents,"icon",icon,strlen(icon)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"root",root,strlen(root)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"mime_type",mimeType,strlen(mimeType)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&contents,"revision",&revision,sizeof(unsigned),0,(void*)&njson_imprimir_unsigned);
+
+	NJson arrayContents[1] = {contents};
+
+	//njson
+	char* sizeJson = "0 bytes";
+	char* hashJson = "37eb1ba1849d4b0fb0b28caf7ef3af52";
+	unsigned bytesJson = 0;
+	char thumbExistsJson = '0';
+	char* revJson = "714f029684fe";
+	char* modifiedJson = "Wed, 27 Apr 2011 22:18:51 +0000";
+	char* pathJson = "/Photos";
+	char isDirJson = '1';
+	char* iconJson = "folder";
+	char* rootJson = "dropbox";
+	unsigned revisionJson = 29007;
+
+	njson_init(&njson,"njson","size",sizeJson,strlen(sizeJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"hash",hashJson,strlen(hashJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"bytes",&bytesJson,sizeof(unsigned),0,(void*)&njson_imprimir_unsigned);
+	njson_dato_agregar(&njson,"thumb_exists",&thumbExistsJson,sizeof(char),0,(void*)&njson_imprimir_boolean);
+	njson_dato_agregar(&njson,"rev",revJson,strlen(revJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"modified",modifiedJson,strlen(modifiedJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"path",pathJson,strlen(pathJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"is_dir",&isDirJson,sizeof(char),0,(void*)&njson_imprimir_boolean);
+	njson_dato_agregar(&njson,"icon",iconJson,strlen(iconJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"root",rootJson,strlen(rootJson)+1,0,(void*)&njson_imprimir_string);
+	njson_dato_agregar(&njson,"contents",arrayContents,sizeof(NJson),1,(void*)&njson_print);
+	njson_dato_agregar(&njson,"revision",&revisionJson,sizeof(unsigned),0,(void*)&njson_imprimir_unsigned);
+
+	njson_print(&njson,NULL,0);
+	//njson_tofile(&njson , "testeo.txt");
 
 	/*
-	*   Hacer 2 func de datoAgregar, una con el json y otra con el dato sig
-	*/
-
-	njson_init(&njson,"PruebaJson","numero1 unsigned",&valorUnsigned,sizeof(unsigned),0,(void*)&njson_imprimir_unsigned);
-	njson_dato_agregar(&njson,"numero2 integer",&valorInteger,sizeof(int),0,(void*)&njson_imprimir_entero);
-	njson_dato_agregar(&njson,"numero3 double",&valorDouble,sizeof(double),0,(void*)&njson_imprimir_double);
-	njson_dato_agregar(&njson,"numero4 float",&valorFlotante,sizeof(float),0,(void*)&njson_imprimir_float);
-	njson_dato_agregar(&njson,"boolean",&valorBoleano,sizeof(char),0,(void*)&njson_imprimir_boolean);
-	njson_dato_agregar(&njson,"texto",valorString,strlen(valorString)+1,0,(void*)&njson_imprimir_string);
-	njson_dato_agregar(&njson,"numero3 otra vez",&valorDouble,sizeof(double),0,(void*)&njson_imprimir_double);
-	/*njson_dato_agregar(&njson,"boolean otra vez",&valorBoleano,sizeof(char),0,(void*)&njson_imprimir_boolean);
-	njson_dato_agregar(&njson,"numero4 otra vez",&valorFlotante,sizeof(float),0,(void*)&njson_imprimir_float);
-	*/
-
 	if	(argc == 3 || argc == 1){
 		//Podemos avanzar
 		if(argc == 1){ //No tenemos parametros OK
 			//njson_print(pnjson);
-			njson_print(&njson);
+			njson_print(&njson,NULL,0);
 		}
 
 		if(argc == 3){ //Tenemos dos parametros -f y nombre archivo
@@ -64,9 +109,11 @@ void main(int argc, char* argv[]) {
 		}
 
 
-	}
+	}*/
 
 	//Termino el programa, libero la memoria del njson para evitar goteo de memoria
+	njson_release(&photoInfo);
+	njson_release(&contents);
 	njson_release(&njson);
 
 
